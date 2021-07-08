5ee5068ad0864b6e9f16395e98c3d3c7f4689a3f
Aryan Kishan <84366895+akishan28@users.noreply.github.com>
2021-07-08 15:30:43 +0530

    Add files via upload

---
+++ cart/src/main/java/com/cart/controller/CartController.java
@@ -0,0 +1,48 @@
+package com.cart.controller;
+
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cart.model.Cart;
+import com.cart.service.CartService;
+
+
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+    @GetMapping("/getcart")
+    public ResponseEntity<List<Cart>> getCart() {
+        List<Cart> cart = cartService.getCart();
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+    @PostMapping("/postcart")
+    public ResponseEntity<Cart> saveCart(@RequestBody Cart cart) {
+        Cart s = cartService.saveCart(cart);
+        return new ResponseEntity<>(s, HttpStatus.CREATED);
+    }
+    @PutMapping("/putcart")
+    public ResponseEntity<Cart> updateCart(@RequestBody Cart cart) {
+        Cart s = cartService.updateCart(cart);
+        return new ResponseEntity<>(s, HttpStatus.CREATED);
+    }
+    @DeleteMapping("/deletecart")
+    public ResponseEntity<String> deleteProduct(@RequestParam(name = "id") String id) {
+        String message = cartService.getCart(id);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
---
+++ cart/src/main/java/com/cart/model/Cart.java
@@ -0,0 +1,52 @@
+package com.cart.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="cart")
+public class Cart {
+	
+	
+	@Id
+	 private String productName;
+	 private long productPrice;
+	 private long productQuantity;
+	 
+	 
+	 public Cart() {
+		 
+	 }
+	 
+	 
+	public Cart(String productName, long productPrice, long productQuantity) {
+		super();
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.productQuantity = productQuantity;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public long getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(long productPrice) {
+		this.productPrice = productPrice;
+	}
+	public long getProductQuantity() {
+		return productQuantity;
+	}
+	public void setProductQuantity(long productQuantity) {
+		this.productQuantity = productQuantity;
+	}
+	@Override
+	public String toString() {
+		return "Cart [productName=" + productName + ", productPrice=" + productPrice + ", productQuantity="
+				+ productQuantity + "]";
+	}
+	 
+	 
+}
